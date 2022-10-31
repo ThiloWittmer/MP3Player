b4056d67223ad0458d82bb747d772c5669eaff09
ThiloWittmer <thilo.wittmer@gmail.com>
2022-10-31 20:32:04 +0100

    init

---
+++ KeyboardController.java
@@ -0,0 +1,9 @@
+public class KeyboardController {
+    public static void main(String[] args) {
+        MP3Player mp3Player;
+        mp3Player = new MP3Player();
+
+
+        mp3Player.play("./mp3files/02 Drei Worte.mp3");
+    }
+}
---
+++ MP3Player.java
@@ -0,0 +1,32 @@
+import de.hsrm.mi.eibo.simpleplayer.SimpleAudioPlayer;
+import de.hsrm.mi.eibo.simpleplayer.SimpleMinim;
+
+/**
+ * MP3Player
+ */
+public class MP3Player {
+
+    private SimpleMinim minim;
+    private SimpleAudioPlayer audioPlayer;
+    
+    public MP3Player() {
+        minim = new SimpleMinim();
+    }
+
+    public void play(String fileName) {
+        audioPlayer = minim.loadMP3File(fileName);
+        audioPlayer.play();
+    }
+
+    public void play() {
+        audioPlayer.play();
+    }
+
+    public void pause() {
+        audioPlayer.pause();
+    }
+
+    public void volume(float value) {
+        audioPlayer.setGain(value);
+    }
+}
